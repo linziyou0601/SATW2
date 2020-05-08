8c04c777a8cc7310b2a0f668a7b4afeee386b78a
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2020-05-08 21:15:40 +0800

    update

--- src/main/java/com/satw/demo/Blockchain/TransactionInput.java
+++ src/main/java/com/satw/demo/Blockchain/TransactionInput.java
@@ -9,11 +9,13 @@ public class TransactionInput {
     }
     // getter
     public String getSourceOutputHash() { return sourceOutputHash; }
+    public String getPayerAddress() { return payerAddress; }
     public int getAmount(){ return amount; };
 
     // operator
     public void processAmount() {
         TransactionOutput UTXO = Blockchain.getUTXOs().get(sourceOutputHash);
+        System.out.println(UTXO.getReceiverAddress());
         payerAddress = (UTXO==null? "": UTXO.getReceiverAddress());
         amount = (UTXO==null? 0: UTXO.getAmount());
     }
