d856a34b592902b75f8893e39a430b57fcca1ada
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2020-05-17 01:18:55 +0800

    update

--- src/main/java/com/satw/demo/Model/Ordered.java
+++ src/main/java/com/satw/demo/Model/Ordered.java
@@ -6,8 +6,7 @@ import com.satw.demo.Normal.Msg;
 public class Ordered implements State {
 	private final String type = "Ordered";
 	public Msg update(Order order, CreateNotifyLambda<String, String, Integer, String, String, String> createNotifyLambda, User loginUser){
-		//orderController.requestSendUnshipNotify(order);//通知未出貨
-		order.notifyUnshipOrder(createNotifyLambda);
+		order.notifyUnshipOrder(createNotifyLambda);   //通知未出貨
 		order.sendOrderDetail();        			   //發送明細
 		order.setState(new Placed());   			   //狀態更新為已付款
 		return new Msg("Successful", "Order state has updated.", "success");
